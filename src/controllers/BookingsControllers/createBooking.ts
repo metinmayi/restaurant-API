2f166ffb7209669c3d87876131f443d58ab2179c
Metin Ahmad <metin.ahmad@paligo.net>
2022-08-18 22:46:47 +0200

    Added controller method for creating bookings

--- src/controllers/BookingsControllers/createBooking.ts
+++ src/controllers/BookingsControllers/createBooking.ts
@@ -1,8 +1,10 @@
-import { Request, Response } from "express";
-import { Document } from "mongoose";
-import { BookingModel } from "../../database/schemas/bookingSchema";
-import { FlamingoResponse } from "../../models/FlamingoResponse";
-import { validateCreateBooking } from "../../validation/BookingsValidation/validateCreateBooking";
+import { Request, Response } from 'express';
+import { Document } from 'mongoose';
+import { BookingModel } from '../../database/schemas/bookingSchema';
+import { FlamingoResponse } from '../../models/FlamingoResponse';
+import { validateCreateBooking } from '../../validation/BookingsValidation/validateCreateBooking';
+import { convertVisitorsToTables } from './BookingsUtils/convertVisitorsToTables';
+import { getAvailableTables } from './BookingsUtils/getAvailableSlots';
 
 /**
  * Creates a new booking in the database
@@ -10,30 +12,44 @@ import { validateCreateBooking } from "../../validation/BookingsValidation/valid
  * @param {Response} res Express Response
  * @returns {Promise<void>}
  */
-export const createBooking = async (req: Request, res: Response): Promise<void> => {
-    const response = new FlamingoResponse();
+export const createBooking = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const response = new FlamingoResponse();
 
-    const validationResponse = validateCreateBooking(req.body);
+  const validationResponse = validateCreateBooking(req.body);
 
-    if (!validationResponse.valid) {
-        response.error = validationResponse.message || 'An error has occured';
-        res.status(400).json(response);
-        return;
+  if (!validationResponse.valid) {
+    response.error = validationResponse.message || 'An error has occured';
+    res.status(400).json(response);
+    return;
+  }
+
+  const Booking = constructBooking(req.body);
+  const { date, time, visitors } = Booking.toJSON();
+
+  try {
+    const availableTables = await getAvailableTables(date, time);
+
+    const requiredTables = convertVisitorsToTables(visitors);
+
+    if (availableTables < requiredTables) {
+      response.error = 'There are not enough available tables';
+      res.status(200).json(response);
+      return;
     }
 
-    const Booking = constructBooking(req.body);
+    await Booking.save();
+    response.message = 'Successfully created a booking';
 
-    try {
-        await Booking.save();
-        response.message = 'Successfully created a booking';
-
-        res.status(200).json(response);
-    } catch (error) {
-        console.log(error);
-        response.error = 'There was an issue connecting to the database';
-        res.status(500).json(response);
-    }
-}
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+    response.error = 'There was an issue connecting to the database';
+    res.status(500).json(response);
+  }
+};
 
 /**
  * Takes the request body and constructs a new Booking object from it.
@@ -41,16 +57,19 @@ export const createBooking = async (req: Request, res: Response): Promise<void> 
  * @returns {Document} Booking Document
  */
 const constructBooking = (reqBody: any): Document => {
-    const {date, time, name, phone, email, visitors} = reqBody;
+  const { date, time, name, phone, email, visitors } = reqBody;
 
-    const Booking = new BookingModel({
-        date,
-        time,
-        name,
-        phone,
-        email,
-        visitors
-    });
+  const tables = convertVisitorsToTables(visitors);
 
-    return Booking;
-}
+  const Booking = new BookingModel({
+    date,
+    time,
+    name,
+    phone,
+    email,
+    visitors,
+    tables,
+  });
+
+  return Booking;
+};
