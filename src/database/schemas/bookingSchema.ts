53dbca8276369fb0538c28602071a47a52f853bd
Metin Ahmad <metin.ahmad@paligo.net>
2022-08-18 22:46:26 +0200

    Updated schema for bookings

--- src/database/schemas/bookingSchema.ts
+++ src/database/schemas/bookingSchema.ts
@@ -1,21 +1,23 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 interface IBooking {
-    date: String,
-    time: Number,
-    name: String,
-    phone: String,
-    email: String,
-    visitors: Number
-};
+  date: string;
+  time: number;
+  name: string;
+  phone: string;
+  email: string;
+  visitors: number;
+  tables: number;
+}
 
 const bookingSchema = new mongoose.Schema<IBooking>({
-    date: {type: String, required: true},
-    time: {type: Number, required: true},
-    name: {type: String, required: true},
-    phone: {type: String, required: true},
-    email: {type: String, required: true},
-    visitors: {type: Number, required: true}
+  date: { type: String, required: true },
+  time: { type: Number, required: true },
+  name: { type: String, required: true },
+  phone: { type: String, required: true },
+  email: { type: String, required: true },
+  visitors: { type: Number, required: true },
+  tables: { type: Number, required: true },
 });
 
 export const BookingModel = mongoose.model('Booking', bookingSchema);
