b14a31548a2f8a02d591f609a88e0492cf1a97ab
Metin Ahmad <metin.ahmad@paligo.net>
2022-08-28 21:29:31 +0200

    Added validation for patching a booking

---
+++ src/validation/BookingsValidation/validatePatchBooking.ts
@@ -0,0 +1,25 @@
+import Joi from "joi";
+import { ValidationResponse } from "../../models/ValidationResponse";
+
+const schema = Joi.object({
+  _id: Joi.string().required(),
+  date: Joi.date().required(),
+  time: Joi.number().valid(18, 21).required(),
+  email: Joi.string().email().required(),
+  tables: Joi.number().required(),
+});
+
+/**
+ * Validates the request body against the excepted schema
+ * @param reqBody
+ * @returns {ValidationResponse}
+ */
+export const validatePatchBooking = (reqBody: any): ValidationResponse => {
+  const result = schema.validate(reqBody);
+
+  if (result.error) {
+    return { valid: false, message: result.error.message };
+  }
+
+  return { valid: true, message: result.error };
+};
