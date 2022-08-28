ea6574a1079c3165db8621bf5605ee493a6d84de
Metin Ahmad <metin.ahmad@paligo.net>
2022-08-28 17:52:00 +0200

    Updated getAllBookings

--- src/controllers/BookingsControllers/getAllBookings.ts
+++ src/controllers/BookingsControllers/getAllBookings.ts
@@ -1,29 +1,29 @@
 import { Request, Response } from "express";
-import "../../database/database"
+import "../../database/database";
 import { BookingModel } from "../../database/schemas/bookingSchema";
 import { FlamingoResponse } from "../../models/FlamingoResponse";
 
-
 /**
- * 
+ * Gets all bookings. Only returns an object with relevant fields.
  * @param {Request} req Express Request
  * @param {Response} res Express Response
- * @returns All bookings in the collection Arra
+ * @returns All bookings in the collection
  */
 export const getAllBookings = async (req: Request, res: Response) => {
-    const response = new FlamingoResponse();
+  const response = new FlamingoResponse();
 
-    try {
-        const bookings = await BookingModel.find();
-        response.message = 'Successfully retrieved all bookings';
-        response.data = bookings;
-        res.status(200).json(response);
-        return;
-    } catch(error) {
-        console.log(error)
-        response.error = "There was an issue connecting to the database";
+  try {
+    const bookings = await BookingModel.find().select(
+      "_id date time email tables"
+    );
+    response.message = "Successfully retrieved all bookings";
+    response.data = bookings;
+    res.status(200).json(response);
+    return;
+  } catch (error) {
+    console.log(error);
+    response.error = "There was an issue connecting to the database";
 
-        res.status(500).json(response);
-    }
-    
-}
+    res.status(500).json(response);
+  }
+};
