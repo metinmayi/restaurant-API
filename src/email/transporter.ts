b6840edb691cc2821b03b16638c2c0d230438c32
Metin Ahmad <metinmayi@gmail.com>
2022-09-03 01:08:28 +0200

    Added transporter for Email

---
+++ src/email/transporter.ts
@@ -0,0 +1,11 @@
+import nodemailer from "nodemailer";
+import "dotenv/config";
+
+export const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  auth: {
+    user: process.env.SMTP_USERNAME,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
